0f45c4fe7f1d8bd50359fcf0cc2c951170c11ab7
TimothyGodwin <timothygodwin1999@gmail.com>
2025-12-21 22:01:19 +0530

    api calls get and POST

--- src/APIexample.jsx
+++ src/APIexample.jsx
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const APIexample = () => {
+  const [userId, setUserId] = useState("5");
+  const [title, setTitle] = useState("ReactJS");
+  const [toDoList, setTodoList] = useState([]);
+
+  const handleFetchTodo = () => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/todos?userId=${userId}`)
+      .then((todoRes) => {
+        console.log("res : ", todoRes);
+        if (todoRes.status == 200) {
+          setTodoList(todoRes.data);
+        } else if (todoRes.status != 200) {
+          setTodoList([]);
+        }
+      })
+      .catch((err) => {
+        console.log("Error : ", err);
+      });
+  };
+
+  const handlePOSTTodo = () => {
+    const payload = {
+      title: title,
+      userId: userId,
+    };
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", payload)
+      .then((res) => {
+        console.log("SAVED : ", res);
+      })
+      .catch((err) => {
+        console.log("ERROR : ", err);
+      });
+  };
+
+  useEffect(() => {
+    handleFetchTodo();
+  }, []);
+
   return (
     <div>
       <p>APIexample</p>
+      <input value={userId} onChange={(e) => setUserId(e.target.value)} />
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <button onClick={() => handleFetchTodo()}>GET UserId</button>
+      <button onClick={() => handlePOSTTodo()}>POST ToDo</button>
+
+      {toDoList.map((item, index) => (
+        <div>
+          <p>{index + 1}.</p>
+          <p>Title : {item.title}</p>
+          <p>id : {item.id}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 };
